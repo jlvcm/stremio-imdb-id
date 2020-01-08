816a49e2a1ec160ce7154d8bdf481b97b7c08dc7
João Melo <joaolvcm@gmail.com>
2020-01-08 17:56:28 +0000

    fix ids with 0s

--- addon.js
+++ addon.js
@@ -39,9 +39,9 @@ builder.defineCatalogHandler(args => {
 	return new Promise((resolve, reject) => {
 		var id;
 		if(args.type == 'search' && args.id=='search_imdb' && args.extra.search){
-			id = args.extra.search.trim().match(/^(tt)?(\d{7,8})$/);
+			id = args.extra.search.trim().match(/^(tt)?(\d{7,})$/);
 			if(id){
-				id = 'tt'+id[2];
+				id = 'tt'+parseInt(id[2], 10);
 				request('https://www.imdb.com/title/'+id, function (error, response, html) {
 					if (!error && response.statusCode == 200) {
 						const $ = cheerio.load(html,{ xmlMode: true });
